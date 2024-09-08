03745177f9f5c61735abaaec533c1cc604293af4
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-08 02:53:00 +0000

    4, 5, 6, 7 - Checking for errors

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+
 for i in range(0, 99):
-    if i >= 10 and i <= 16:
-        print("{}".format(i), "= 0x{}".format(chr(i)))
+    if i >= 10 and i <= 15:
+        print("{} =".format(i), "{}".format(hex(i)))
+
     else:
         print("{}".format(i), "= 0x{}".format(i))
+
+# for i in range(0, 10):
+#     print("{}".format(i), "= 0x{}".format(i))
+# for i in range(10, 16):
+#     print("{}".format(i), "= {}".format(hex(i)))
+# for i in range(16, 100):
+#     for x in range(10, 63):
+#         print("{}".format(i), "= 0x{}".format(x))
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    letra = ord(c)
+    if letra >= 97 and letra <= 122:
+        return True
+    else:
+        return False
