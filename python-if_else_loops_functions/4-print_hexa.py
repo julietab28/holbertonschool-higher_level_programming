9519b3b23f44f214190595ed248a13fee1bb42fa
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-09 19:01:49 +0000

    4 - Fixed

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,16 +2,3 @@
 
 for i in range(0, 99):
     print("{} =".format(i), "{}".format(hex(i)))
-    # if i >= 10 and i <= 15:
-    #     print("{} =".format(i), "{}".format(hex(i)))
-
-    # else:
-    #     print("{}".format(i), "= 0x{}".format(i))
-
-# for i in range(0, 10):
-#     print("{}".format(i), "= 0x{}".format(i))
-# for i in range(10, 16):
-#     print("{}".format(i), "= {}".format(hex(i)))
-# for i in range(16, 100):
-#     for x in range(10, 63):
-#         print("{}".format(i), "= 0x{}".format(x))
