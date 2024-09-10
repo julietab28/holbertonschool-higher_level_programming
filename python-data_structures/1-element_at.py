56994e6c0c4e8e9907d518d59653effb9f373fa6
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-10 19:54:17 +0000

    1 - Trying to solve this problem

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in range(len(my_list)):
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            print("Element an index {:d} is {}".format(idx, my_list[i]))
