28a2b250fddf0a39399f58fb42846d0b823cf1fa
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-10 18:18:25 +0000

    9 - Trying to solve this problem

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        if number >= 10000 or number <= -10000:
+            last_digit = ((((number % 10000) % 1000) % 100) % 10)
+        elif number >= 1000 or number <= -1000:
+            last_digit = (((number % 1000) % 100) % 10)
+        elif number >= 100 or number <= -100:
+            last_digit = ((number % 100) % 10)
+        else:
+            last_digit = number % 10
+
+        if last_digit > 5:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_digit), "and is greater than 5")
+        elif last_digit < 6 and last_digit != 0:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_digit), "and is less than 6 and not 0")
+        elif last_digit == 0:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_digit), "and is 0")
+    else:
+        if number <= -10000:
+            last_neg_digit = ((((number % -10000) % -1000) % -100) % -10)
+        elif number <= -1000:
+            last_neg_digit = (((number % -1000) % -100) % -10)
+        elif number <= -100:
+            last_neg_digit = ((number % -100) % -10)
+        else:
+            last_neg_digit = number % -10
+
+        if last_neg_digit > 5:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_neg_digit), "and is greater than 5")
+        elif last_neg_digit < 6 and last_neg_digit != 0:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_neg_digit),
+                "and is less than 6 and not 0")
+        elif last_neg_digit == 0:
+            print("Last digit of {}".format(number),
+                "is {}".format(last_neg_digit), "and is 0")
