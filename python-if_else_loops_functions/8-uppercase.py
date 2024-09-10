49f1ab8a73cfd0182b1da62269d8f436a7305a13
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-10 17:59:42 +0000

    8 - Solving many problems

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    for i in 
+    palabra = ""
+    for letter in str:
+        if ord(letter) >= 65 and ord(letter) <= 90:
+            palabra += letter
+        elif ord(letter) == 32:
+            palabra += letter    
+        else:
+            palabra += chr(ord(letter) - 32)
+    return palabra
