dcd3418486c795a04a3763de63d65ea663fe7860
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-11 17:22:49 +0000

    4 - Replacing an element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,7 +6,6 @@ def new_in_list(my_list, idx, element):
     elif idx > (len(my_list) - 1):
         return my_list
     else:
-        for x in range(0, len(my_list)):
-            if x == idx:
-                new_list[x] = element
+        new_list = my_list[:]
+        new_list[idx] = element
         return new_list
