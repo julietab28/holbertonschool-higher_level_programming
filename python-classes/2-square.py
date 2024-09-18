52111d206c94af28ae1288130985ca9103267157
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-18 17:57:53 +0000

    3 - Problems fixed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
 
     Atributos:
         __size (int): Tamaño del cuadrado. Debe ser un entero no negativo
-    
+
     Métodos:
         __init__(self, size=0): Inicializa una instancia de la clase Square
         is_int(self): Valida si el tamaño es un entero no negativo
@@ -22,7 +22,7 @@ class Square:
 
         Args:
             size (int): El tamaño del lado del cuadrado. Por defecto es 0
-        
+    
         Raises:
             ValueError: Si size no es un entero o es menor que 0
         """
@@ -31,5 +31,5 @@ class Square:
         else:
             if size < 0:
                 raise ValueError("size must be >= 0")
-        
+
         self.__size = size
