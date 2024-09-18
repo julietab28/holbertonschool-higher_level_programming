a81c99f121fe76b6acc0b73f7ff83b32c94243c2
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-18 16:20:32 +0000

    2 - Initializing a new attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Este modulo crea una clase Square
+"""
+
+
+class Square:
+    """
+    Clase vacia
+
+    Atributos:
+        size (None): el size de un square
+    """
+    def __init__(self, size):
+        """
+        Inicializacion de un nuevo Square
+
+        Args:
+            size (None): el size de un square
+        """
+        self.__size = size
