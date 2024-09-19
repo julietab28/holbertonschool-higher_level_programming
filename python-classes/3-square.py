053fda13428f59d3397d516ed6a0ffed968250a3
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-19 19:19:01 +0000

    4 - Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -31,5 +31,5 @@ class Square:
                 raise ValueError("size must be >= 0")
 
         self.__size = size
-    def area(size):
-        return self.size * self.size
+    def area(self):
+        return self.__size * self.__size
