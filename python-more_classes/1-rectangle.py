b2caf6d7558249a08ab2c9968acef9c9dc867f4c
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-22 15:59:08 +0000

    2 - Getter and setter

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Creacion de una clase Rectangulo que representa un rectangulo
+"""
+
+
+class Rectangle:
+    """
+    Clase que representa un rectangulo
+    """
+    def __init__(self, width = 0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter. Permite modificar el atributo, le asignamos un nuevo valor a width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter
+        """
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """
+        Setter. Permite modificar el atributo, le asignamos un nuevo valor a height 
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
