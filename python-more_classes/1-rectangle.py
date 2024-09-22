e6a47b1a83618f37532fb824d1936b34736f5ce8
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-22 17:10:48 +0000

    2 - Getter and setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,7 +9,18 @@ class Rectangle:
     Clase que representa un rectangulo
     """
     def __init__(self, width = 0, height=0):
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        else:
+            if self.__width < 0:
+                raise ValueError("width must be >= 0")
         self.__width = width
+
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        else:
+            if self.__height < 0:
+                raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
