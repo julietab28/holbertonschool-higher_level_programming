a3e04fed19362c20955eb6d7a48383bff93974ef
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-23 00:44:27 +0000

    5 - Repr and eval

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -66,5 +66,6 @@ class Rectangle:
             return ""
         else:
             return "\n".join('#' * self.__width for _ in range(self.__height))
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
