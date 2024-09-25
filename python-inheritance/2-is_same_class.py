2cd5811c198958ffd720f399ddbdd63ed911d12c
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-25 14:09:17 +0000

    2 - Is instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,8 +4,8 @@ Módulo que define la clase a_class
 """
 
 
-    def is_same_class(obj, a_class):
-        """
-        is_same_class
-        """
-        return isinstance(obj, a_class)
+def is_same_class(obj, a_class):
+    """
+    is_same_class
+    """
+    return isinstance(obj, a_class)
