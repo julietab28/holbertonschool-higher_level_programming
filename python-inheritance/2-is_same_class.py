c57256a55d1dee2caf0f8d2afd6105336775ecf4
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-25 13:59:21 +0000

    2  - Is instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """
-Class
+Módulo que define la clase a_class
 """
 
 
 class a_class:
     """
-    Class
+    Clase que proporciona un método para verificar si un objeto
+    es una instancia de una clase específica
     """
     def is_same_class(obj, a_class):
         """
-        is_same_class
+        Verifica si un objeto es exactamente de la clase especificada
+
+        Args:
+            obj: El objeto a verificar
+            a_class: La clase con la que se desea comparar el objeto
+
+        Returns:
+            bool: True si 'obj' es una instancia de 'a_class', False en caso contrario
         """
         return isinstance(obj, a_class)
