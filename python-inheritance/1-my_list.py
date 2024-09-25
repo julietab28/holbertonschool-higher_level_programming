e8f6b2a535491f275b0649afac83012c27462110
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-25 13:27:03 +0000

    1 - Inheritance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Class
+"""
+
+
+class MyList(list):
+    """
+    Class
+    """
+    def print_sorted(self):
+        """
+        print_sorted
+        """
+        print(sorted(self, reverse=False))
