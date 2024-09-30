ca8e87c4ade87756d2bf01d833bff6686bf9e9bb
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-09-30 19:13:12 +0000

    4 - Obj is an instance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,13 +6,15 @@ Módulo que define la función is_kind_of_class.
 
 def is_kind_of_class(obj, a_class):
     """
-    Verifica si un objeto es una instancia de una clase o de una subclase.
+    Verifica si un objeto es una instancia de una clase o 
+    de una subclase.
 
     Args:
         obj: El objeto a verificar.
         a_class: La clase con la que se desea comparar el objeto.
 
     Returns:
-        bool: True si 'obj' es una instancia de 'a_class' o de una subclase de 'a_class', False en caso contrario.
+        bool: True si 'obj' es una instancia de 'a_class' o de 
+        una subclase de 'a_class', False en caso contrario.
     """
     return isinstance(obj, a_class)
