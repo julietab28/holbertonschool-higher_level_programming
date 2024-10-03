46afe02a8ac38865e9ea184d1f339aa3852f4ad3
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-03 20:48:01 +0000

    7 - Method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Modulo de una clase
+"""
+
+
+class BaseGeometry:
+    """
+    Clase que representa geometria
+    """
+    def area(self):
+        """
+        Metodo de area
+        """
+        raise Exception("area() is not implemented")    
