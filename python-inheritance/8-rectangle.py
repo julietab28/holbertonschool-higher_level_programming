b16ebfc00eb5c36bf94496eb786eebf3e84bb49d
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-03 21:16:21 +0000

    9 - Width and height validation

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Modulo de una clase
+"""
+
+
+class BaseGeometry:
+    """
+    Clase que representa geometria
+    """
+    def __init__(self, width, height):
+        """
+        Inicializamos valores
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Metodo de area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Valida si el value es de tipo integer y si es mayor que 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
