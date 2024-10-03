88e34a66458652c0572577d75cf63dc00a1f7bb6
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-03 21:38:21 +0000

    10 - Inheritance and calculatin area

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -24,26 +24,26 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
+    """
+    Clase que hereda de BaseGeometry, representa un rectangulo
+    """
+    def __init__(self, width, height):
         """
-        Clase que hereda de BaseGeometry, representa un rectangulo
+        Inicializamos valores
         """
-        def __init__(self, width, height):
-            """
-            Inicializamos valores
-            """
-            self.integer_validator("width", width)
-            self.__width = width
-            self.integer_validator("height", height)
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
-        def area(self):
-            """
-            Calcula el area de un rectangulo
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        Calcula el area de un rectangulo
+        """
+        return self.__width * self.__height
 
-        def __str__(self):
-            """
-            Restorna un mensaje con la descripcion del rectangulo
-            """
-            return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+    def __str__(self):
+        """
+        Restorna un mensaje con la descripcion del rectangulo
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
