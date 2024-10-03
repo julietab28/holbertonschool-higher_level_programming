e3214893da4d80deb8c2792f68272904a9c84dd2
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-03 20:59:04 +0000

    8 - Validating the value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Modulo de una clase
+"""
+
+
+class BaseGeometry:
+    """
+    Clase que representa geometria
+    """
+    def area(self):
+        """
+        Metodo de area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Valida si el value es de tipo integer y si es mayor que 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be grater than 0".format(name))
