4cd5ce7a0d24466a1062fb57ed491d229991b957
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-07 17:03:57 +0000

    9 - Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,24 +4,7 @@ Modulo de una clase
 """
 
 
-class BaseGeometry:
-    """
-    Clase que representa geometria
-    """
-    def area(self):
-        """
-        Metodo de area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Valida si el value es de tipo integer y si es mayor que 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+Rectangle = __import__('8-rectangle').Rectangle
 
 class Rectangle(BaseGeometry):
         """
