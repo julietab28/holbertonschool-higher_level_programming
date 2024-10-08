666e95e5d68f89f1d5d1be0637f25a630d31199d
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-08 17:51:32 +0000

    Task 3 - Counted Iterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Mtodo de clase
+"""
+
+
+class CountedIterator:
+    def __init__(self, i):
+        self.iterator = iter(i)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.conuter += 1
+        return item
