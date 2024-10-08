c68511b8bba7ced8dd09181292b6d038d1f8278c
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-08 13:08:59 +0000

    Task 2 - Class Verbose List

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Modulo de clase Shape
+"""
+
+
+class VerboseList(list):
+    """
+    Clase 
+    """
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, item):
+        super.pop(item)
+        print("Popped [{}] from the list.".format(item))
