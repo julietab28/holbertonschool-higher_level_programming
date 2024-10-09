d775703662839841ab6b1495a3c1527ab67bf10b
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-09 14:16:42 +0000

    2 - Append a text into the end of a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Modulo
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Funcion que agrega una cadena de texto
+    al final de un archivo
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        file = f.write
+        return len(text)
