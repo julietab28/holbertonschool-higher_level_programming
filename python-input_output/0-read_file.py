b5c57c2913fc0e0a1e698f73dd26e9ebd0344838
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-09 13:32:31 +0000

    0 - Reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Modulo
 
 
 def read_file(filename=""):
+    """
+    Funcion que lee un archivo y luego lo imprime
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         file = f.read()
         print("{}".format(file))
