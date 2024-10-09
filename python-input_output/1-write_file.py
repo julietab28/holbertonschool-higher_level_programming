329ca152b5db59560d5e2fb8594e2540b11a0a7c
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-09 13:48:55 +0000

    1 - Writing into a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     Funcion que escribe dentro de un archivo un
     texto, y luego retorna el numero de
-    caracteres del texto 
+    caracteres del texto
     """
     with open(filename, 'w', encoding="utf-8") as f:
         file = f.write(text)
