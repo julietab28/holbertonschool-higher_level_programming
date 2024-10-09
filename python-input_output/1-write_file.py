16a8fbb5213098e59d90cbed4b9e747a7146d8e2
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-09 13:48:18 +0000

    1 - Writing into a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,11 @@ Modulo 2
 
 
 def write_file(filename="", text=""):
+    """
+    Funcion que escribe dentro de un archivo un
+    texto, y luego retorna el numero de
+    caracteres del texto 
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         file = f.write(text)
         return len(text)
