18e866b506179c4c6cddaf4550fe469bb13ff7ab
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-09 15:53:00 +0000

    4 - from JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/python3
+"""
+Modulo 5
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Funcion que convierte un string JSON
+    a un objeto de Python
+    """
+    x = json.loads(my_str)
+    return x
