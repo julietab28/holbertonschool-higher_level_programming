9b73f8af360ed561b000f926bc8fd8ecc1d22cad
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-11 14:50:47 +0000

    Task 1 - Requests

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -18,10 +18,10 @@ def fetch_and_save_posts():
     if resp.status_code == 200:
         dic = resp.json()
 
-    with open('post.csv', 'w', encoding="utf-8") as csvfile:
-        fieldnames = ["id", "title", "body"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+        with open('post.csv', 'w', encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
 
-        for i in dic:
-            writer.writerow({'id': i["id"], 'title' : i["title"], 'body' : i["body"]})
+            for i in dic:
+                writer.writerow({'id': i["id"], 'title' : i["title"], 'body' : i["body"]})
