52e367a0ebdae0f530b29a63679f7f29d5c8a0c3
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-11 14:41:32 +0000

    Task 1 - Requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests, json, csv
+
+def fetch_and_print_posts():
+    resp = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    print("Status code: {}".format(resp.status_code))
+
+    if resp.status_code == 200:
+        dic = resp.json()
+        for i in dic:
+            print(i["title"])
+
+def fetch_and_save_posts():
+    resp = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if resp.status_code == 200:
+        dic = resp.json()
+
+    with open('post.csv', 'w', encoding="utf-8") as csvfile:
+        fieldnames = ["id", "title", "body"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in dic:
+            writer.writerow({'id': i["id"], 'title' : i["title"], 'body' : i["body"]})
