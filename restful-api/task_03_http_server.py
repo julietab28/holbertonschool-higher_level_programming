12a7bcca60d5ac4c5206eead60c8f5b39ddf7019
julietab28 <136987530+julietab28@users.noreply.github.com>
2024-10-11 19:34:00 +0000

    Task 3 - HTTP Server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,11 +6,32 @@ PORT = 8000
 
 #esta es la api
 class Server(http.server.BaseHTTPRequestHandler):
+    dic_datos = {
+        "name": "John", 
+        "age": 30, 
+        "city": "New York"
+    }
+
+    json_datos = json.dumps(dic_datos) 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(self.json_datos.encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(bytes("OK", "utf-8"))
+        else:
+            self.send_response(404)
+            self.end_headers()
 
 with socketserver.TCPServer(("", PORT), Server) as server:
     server.serve_forever()
