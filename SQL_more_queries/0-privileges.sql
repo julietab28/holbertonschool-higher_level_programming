6ce109740afaa76be7a2e394cffccab71e0311ac
Juli <julietabobadilla123@gmail.com>
2024-10-30 10:43:02 -0300

    0 - Show privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --
-SHOW GARANT FOR 'user_0d_1'@'localhost';
-SHOW GARANT FOR 'user_0d_2'@'localhost';
+SHOW GRANT IF EXIST FOR 'user_0d_1'@'localhost';
+SHOW GRANT IF EXIST FOR 'user_0d_2'@'localhost';
