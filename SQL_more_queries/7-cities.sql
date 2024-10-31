a518a0b84b99543bb07bb28ed995afaffd7090bf
Juli <julietabobadilla123@gmail.com>
2024-10-31 17:47:37 -0300

    7 - Cities and foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,8 +5,7 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    state_id INT 
+    state_id int NOT NULL FOREIGN KEY REFERENCES states(id),
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    PRIMARY KEY(id)
 )
